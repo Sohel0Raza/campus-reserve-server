97b4e6e4567d84106748fd9e4cc4ed3d2c3028a8
Sohel0Raza <soheldot1239@gmail.com>
2024-08-21 01:15:25 +0600

    add user api

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 
 //middleware
+dotenv.config();
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q6zwl04.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,22 +28,49 @@ async function run() {
 
     const collegeCollection = client.db("campusDB").collection("colleges");
     const reviewCollection = client.db("campusDB").collection("review");
+    const usersCollection = client.db("campusDB").collection("users");
 
     app.get("/colleges", async (req, res) => {
-        const cursor = collegeCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      });
+      const cursor = collegeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/colleges/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await collegeCollection.findOne(query);
+      res.send(result);
+    });
 
     app.get("/review", async (req, res) => {
-        const cursor = reviewCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      });
-    
+      const cursor = reviewCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // user related api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user alrady exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -52,9 +79,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Campus Reserve server is running...");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Campus Reserve server is running on port:${port}`);
-  });
+  res.send("Campus Reserve server is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Campus Reserve server is running on port:${port}`);
+});
